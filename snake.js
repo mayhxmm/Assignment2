279be95c04cb83c1972529cb346b733e4d2d0e86
mayhxmm <55783121+mayhxmm@users.noreply.github.com>
2019-11-23 17:06:55 +0800

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,139 @@
+var hasWon = false;
+window.rollDice = ()=>{
+  if (hasWon) {
+    return;
+  }
+  const max = 6;
+  const roll = Math.ceil(Math.random() * max);
+  let currentPlayer = players[currentPlayerTurn];
+  console.log( currentPlayer.name +" rolled", roll);
+  currentPlayer.position += roll;
+  ladders.forEach(ladder=>{
+    if (ladder.start === currentPlayer.position) {
+      console.log(currentPlayer.name + " has stepped on a ladder!");
+      currentPlayer.position = ladder.end;
+    }
+  });
+
+  if (currentPlayer.position >= position) {
+    console.log( currentPlayer.name + " has won!");
+    hasWon = true;
+  }
+  if (currentPlayer.position === position) {
+    const diff = currentPlayer.position - position;
+    currentPlayerPosition = position - diff;
+  }
+
+  currentPlayerTurn ++;
+  if (currentPlayerTurn >= players.length) {
+    currentPlayerTurn = 0;
+  }
+  renderBoard();
+}
+
+const players = [{
+  name:"Yellow",
+  position: 0,
+  color: "gold"
+},{
+  name:"White",
+  position: 0,
+  color: "white"
+}];
+
+let currentPlayerTurn = 0;
+
+const width = 5;
+const height = 5;
+const board = [];
+let position = 0;
+let blackSquare = false;
+const ladders = [{
+  start: 7,
+  end: 14
+},{
+  start: 18,
+  end: 23
+},{
+  start: 14,
+  end: 4
+},{
+  start: 22,
+  end: 15
+}];
+
+for (var y = height; y >= 1; y--) {
+  let row = [];
+
+  board.push(row);
+  for (var x = 0; x < width; x++) {
+
+    row.push({x,y,occupied:null,position,color: blackSquare ? "lightskyblue" : "lightpink"});
+    blackSquare = !blackSquare;
+    position ++;
+  }
+}
+
+const boardSizeConst = 50;
+const renderBoard = ()=>{
+  let boardHTML = ``;
+  board.forEach(row=>{
+    row.forEach(square=>{
+      boardHTML += `<div class=square style="top:${square.y * boardSizeConst}px; left:${square.x * boardSizeConst}px; background-color:${square.color}"></div>`
+    });
+  });
+
+  players.forEach(player=>{
+    let square = null;
+    board.forEach(row=>{
+    row.forEach(square=>{
+          if (square.position === player.position) {
+            boardHTML += `<div class=player style="top:${square.y * boardSizeConst + 5}px; left:${square.x * boardSizeConst + 5}px;background-color:${player.color}"></div>`;
+          }
+      });
+    });
+  });
+
+  ladders.forEach(ladder=>{
+
+    //let start = 0;
+    let startPos = {x:0,y:0};
+    let endPos = {x:0,y:0};
+
+    board.forEach(row=>{
+      row.forEach(square=>{
+        if (square.position === ladder.start) {
+          startPos.x = square.x * boardSizeConst;
+          startPos.y = square.y * boardSizeConst;
+        }
+
+        if (square.position === ladder.end) {
+          endPos.x = square.x * boardSizeConst;
+          endPos.y = square.y * boardSizeConst;
+        }
+      });
+    });
+
+    const isLadder = ladder.end > ladder.start;
+
+    drawLine({color : isLadder ? "green" : "red",startPos,endPos});
+  });
+  document.getElementById("board").innerHTML = boardHTML;
+}
+
+function drawLine({color,startPos,endPos}){
+
+  var c=document.getElementById("canvas");
+  var ctx=c.getContext("2d");
+
+  ctx.beginPath();
+  const sizeRatio = 1;
+  ctx.moveTo(startPos.x + 25,startPos.y + 25);
+  ctx.lineTo(endPos.x + 25,endPos.y + 25 );
+
+  ctx.lineWidth = 15;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
+
+renderBoard();
